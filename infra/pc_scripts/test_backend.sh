5f63e1917d34c379e35beb671b8c028eb5e9b957
cgaunet <gaunet.cyril@gmail.com>
2021-08-17 12:06:06 +0200

    wip

--- infra/pc_scripts/test_backend.sh
+++ infra/pc_scripts/test_backend.sh
@@ -6,8 +6,10 @@ function test_backend {
     else
         PYTEST_ARGS=$*
     fi
-    RUN='docker-compose -f "$ROOT_PATH"/docker-compose-app.yml restart postgres-test;
+    RUN='docker stop pc-postgres-pytest;
+        docker rm pc-postgres-pytest;
+        docker-compose -f "$ROOT_PATH"/docker-compose-app.yml up -d postgres-test;
         while ! docker-compose -f "$ROOT_PATH"/docker-compose-app.yml logs postgres-test 2> /dev/null | grep -q "PostgreSQL init process complete; ready for start up"; do echo "waiting for test-database"; sleep 0.5; done;
-        docker exec pc-flask bash -c "cd /opt/services/flaskapp/src/ && rm -rf static/object_store_data/thumbs/* && RUN_ENV=tests pytest --durations=5 --color=yes -rsx -v $PYTEST_ARGS"
+        rm -rf static/object_store_data/thumbs/* && RUN_ENV=tests pytest --durations=5 --color=yes -rsx -v -p no:warnings $PYTEST_ARGS
         '
 }
