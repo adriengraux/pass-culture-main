5562c52948e82efafd5c39c46b3adbc7a2d6795b
Abderahmane KARKI <abdel.karki@gmail.com>
2022-02-10 15:22:32 +0100

    [PC-12524](API) Add Pre and Post Deployment Branches for migrations

---
+++ api/src/pcapi/alembic/versions/20220207T154955_d3da2eac435d_fork_pre_deploy.py
@@ -0,0 +1,16 @@
+"""fork pre-deploy
+"""
+
+# revision identifiers, used by Alembic.
+revision = "d3da2eac435d"
+down_revision = "70946e8b0622"
+branch_labels = ("pre",)
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
---
+++ api/src/pcapi/alembic/versions/20220207T155219_ffafc4d7210f_fork_post_deploy.py
@@ -0,0 +1,17 @@
+"""fork post-deploy
+"""
+
+
+# revision identifiers, used by Alembic.
+revision = "ffafc4d7210f"
+down_revision = "70946e8b0622"
+branch_labels = ("post",)
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
